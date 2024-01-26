0012929a94bd93021d4d7d486f1db68060f705f2
Angelo <jellotinandrade@gmail.com>
2024-01-26 04:29:43 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,19 +1,23 @@
-let carbonFootprintPoint = 0;
-const numberInHousehold = 3;
-
-if (numberInHousehold === 1) {
-    carbonFootprintPoint = carbonFootprintPoint + 14;
-} else if (numberInHousehold === 2) {
-    carbonFootprintPoint = carbonFootprintPoint + 12;
-} else if (numberInHousehold === 3) {
-   carbonFootprintPoint = carbonFootprintPoint + 10;
-} else if (numberInHousehold === 4) {
-    carbonFootprintPoint = carbonFootprintPoint + 8;
-} else if (numberInHousehold === 5) {
-    carbonFootprintPoint = carbonFootprintPoint + 6;
-} else if (numberInHousehold  > 6) {
-    carbonFootprintPoint = carbonFootprintPoint + 4;
-} else if (numberInHousehold > 6){
-    carbonFootprintPoint = carbonFootprintPoint + 2;
+function determineHouseholdPts(numberInHousehold) {
+    if (numberInHousehold === 1) {
+        carbonFootprintPoint = carbonFootprintPoint + 14;
+    } else if (numberInHousehold === 2) {
+        carbonFootprintPoint = carbonFootprintPoint + 12;
+    } else if (numberInHousehold === 3) {
+       carbonFootprintPoint = carbonFootprintPoint + 10;
+    } else if (numberInHousehold === 4) {
+        carbonFootprintPoint = carbonFootprintPoint + 8;
+    } else if (numberInHousehold === 5) {
+        carbonFootprintPoint = carbonFootprintPoint + 6;
+    } else if (numberInHousehold  > 6) {
+        carbonFootprintPoint = carbonFootprintPoint + 4;
+    } else if (numberInHousehold > 6){
+        carbonFootprintPoint = carbonFootprintPoint + 2;
+    }
+    console.log(`Based on the number of members in the household ${numberInHousehold} the points would be ${carbonFootprintPoint}`);
 }
-console.log(`Based on the number of members in the household ${numberInHousehold} the points would be ${carbonFootprintPoint}`)
+
+let carbonFootprintPoint = 0;
+//const numberInHousehold = 3;
+
+determineHouseholdPts(3)
